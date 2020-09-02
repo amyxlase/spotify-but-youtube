0cdd3883eac20b66c1c0f5b6e3f3b93a714593e0
amyxlase <31298710+amyxlase@users.noreply.github.com>
2020-09-02 09:18:37 -0400

    setting up pages

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.js';
+
+function Home() {
+
+    return (
+            <p>Welcome! Please <a href="/login">log in</a>.</p>
+        
+
+    );
+    
+} export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function Login() {
+
+    return (
+        <a href="/login/google">Log In with google</a>
+
+    );
+    
+} export default Login;
