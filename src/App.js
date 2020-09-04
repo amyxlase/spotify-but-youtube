f7c6563ca557e8a3e4b1c42ada0bdd4c82b8ba1f
amyxlase <31298710+amyxlase@users.noreply.github.com>
2020-09-03 20:47:20 -0400

    working on react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
-import Home from './compoments/Home';
+import Login from '..components/Login.js';
+import Home from './compoments/Home.js';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
         <Route path="/login" component={Login} />
-        <Route path="/login" component={Home} />
+        <Route path="/home" component={Home} />
     </Router>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.js';
 
 function Home() {
 
